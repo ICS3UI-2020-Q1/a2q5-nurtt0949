307ac3cafca7b83bdea5ae8d11bd03c49eff83fd
nurtt0949 <nurtt0949@wrdsb.ca>
2020-09-15 21:05:52 +0000

    FizzBuzz game

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ *FizzBuzz program
+ * @author Tolga Nurtekin
  */
 public class Main {
 
@@ -9,7 +11,33 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    //create a Scanner for user input
+    Scanner input = new Scanner(System.in);
+
+
+    //ask user to submit a number
+    System.out.println("Enter a number to play FizzBuzz");
+
+    //get number from user
+    int number = input.nextInt();
+
+    //figure out what they have to say
+    if(number % 3 == 0 && number % 5 == 0){
+    System.out.println("You should say FizzBuzz");  
+    }else if (number % 3 == 0){
+      System.out.println("You should say Fizz");
+    }else if (number % 5 == 0 ){
+      System.out.println("You should say Buzz");
+    }else{
+      System.out.println("You should say " + number);
+    }
   }
-}
+
+    
+
+    
+    
+   
+    }
+    
+
